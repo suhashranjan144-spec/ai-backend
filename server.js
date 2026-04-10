de974d2ad39919725745764d6bbcc2413d03515f
suhashranjan144-spec <suhashranjan144@gmail.com>
2026-04-10 22:28:01 +0530

    Fix import path for GoogleGenerativeAI

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { GoogleGenerativeAI } from "@google/genai";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 dotenv.config();
 
